6f8a4ff716be06d59d17c1393189619972673259
unknown <k25650727@gmail.com>
2024-06-12 11:52:55 +0200

    commit

--- src/test/java/com/example/utils/Utils.java
+++ src/test/java/com/example/utils/Utils.java
@@ -1,16 +1,21 @@
 package com.example.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Utils {
     public static boolean Is2DListEqualTo2DArray(List<List<Integer>> list, int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (list.get(i).get(j) != arr[i][j]) {
-                    return false;
-                }
-            }
+        List<List<Integer>> arrList = new ArrayList<>();
+        for (int[] num : arr) {
+            List<Integer> tmp = Arrays.stream(num).boxed().toList();
+            arrList.add(tmp);
         }
-        return true;
+        try {
+            return arrList.containsAll(list);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
