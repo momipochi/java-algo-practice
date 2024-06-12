61d37a73a1704e6b23670e863139204cfebcba4c
unknown <k25650727@gmail.com>
2024-06-12 10:47:07 +0200

    commit

--- src/main/java/com/example/permutations/Permutations.java
+++ src/main/java/com/example/permutations/Permutations.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
+//https://leetcode.com/problems/permutations/description/
+//Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 public class Permutations {
     public static List<List<Integer>> permute(int[] nums) {
         boolean[] used = new boolean[nums.length];
