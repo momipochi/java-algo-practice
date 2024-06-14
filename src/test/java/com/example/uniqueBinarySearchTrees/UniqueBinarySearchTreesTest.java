f644a5e6c19633ccbf94254d55820734fcd5542c
unknown <k25650727@gmail.com>
2024-06-14 16:00:13 +0200

    commit

---
+++ src/test/java/com/example/uniqueBinarySearchTrees/UniqueBinarySearchTreesTest.java
@@ -0,0 +1,15 @@
+package com.example.uniqueBinarySearchTrees;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class UniqueBinarySearchTreesTest {
+    @Test
+    public void testNumTrees() {
+        int c = 3;
+        int a = 5;
+
+        assertEquals(a, UniqueBinarySearchTrees.numTrees(c));
+    }
+}
