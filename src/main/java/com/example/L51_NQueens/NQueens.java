1df2f003c43e49efcff2b9affb96bac40278d425
unknown <k25650727@gmail.com>
2024-06-15 16:03:42 +0200

    commit

--- src/main/java/com/example/L51_NQueens/NQueens.java
+++ src/main/java/com/example/L51_NQueens/NQueens.java
@@ -1,48 +1,55 @@
 package com.example.L51_NQueens;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 // https://leetcode.com/problems/n-queens/
 
 public class NQueens {
-    List<List<String>> res = new ArrayList<>();
-    int[] colNumber;
-    Map<Integer, String> map = new HashMap<>();
-
     public List<List<String>> solveNQueens(int n) {
-        colNumber = new int[n];
-        char[] c = new char[n];
-        Arrays.fill(c, '.');
-        for (int i = 0; i < n; i++) {
-            c[i] = 'Q';
-            map.put(i, new String(c));
-            c[i] = '.';
-        }
-        dfs(n, 0, 0, 0, 0);
+        List<List<String>> res = new ArrayList<>();
+        start(res, new ArrayList<>(), 0, n);
         return res;
     }
 
-    private void dfs(int n, int row, int colOccupy, int diagonalOccupy1, int diagonalOccupy2) {
+    private void start(List<List<String>> res, List<Integer> pos, int row, int n) {
         if (row == n) {
-            List<String> t = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                t.add(map.get(colNumber[i]));
+            res.add(formString(pos));
+        }
+        for (int col = 0; col < n; col++) {
+            if (isValid(pos, row, col)) {
+                pos.add(col);
+                start(res, pos, row + 1, n);
+                pos.remove(pos.size() - 1);
             }
-            res.add(t);
-            return;
         }
-        int availablePositions = ((1 << n) - 1) & (~(colOccupy | diagonalOccupy1 | diagonalOccupy2));
-        while (availablePositions != 0) {
-            int position = availablePositions & (-availablePositions);
-            availablePositions = availablePositions & (availablePositions - 1);
-            int col = Integer.bitCount(position - 1);
-            colNumber[row] = col;
-            dfs(n, row + 1, colOccupy | position, (diagonalOccupy1 | position) << 1, (diagonalOccupy2 | position) >> 1);
+    }
+
+    private boolean isValid(List<Integer> pos, int row, int col) {
+        for (int rowTaken = 0; rowTaken < pos.size(); rowTaken++) {
+            int colTaken = pos.get(rowTaken);
+            if (col == colTaken || Math.abs(row - rowTaken) == Math.abs(col - colTaken)) {
+                return false;
+            }
         }
+        return true;
+    }
+
+    private List<String> formString(List<Integer> pos) {
+        List<String> res = new ArrayList<>();
+        int n = pos.size();
+        for (int i : pos) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                if (j == i) {
+                    sb.append("Q");
+                } else {
+                    sb.append(".");
+                }
+            }
+            res.add(sb.toString());
+        }
+        return res;
     }
 
 }
