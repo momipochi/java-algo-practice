7e4e8e123c163f1eff91f6f6ded52b23952bac58
unknown <k25650727@gmail.com>
2024-06-26 13:28:12 +0200

    commit

--- src/main/java/com/example/L46_permutations/Permutations.java
+++ src/main/java/com/example/L46_permutations/Permutations.java
@@ -1,35 +1,32 @@
 package com.example.L46_permutations;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Stack;
 
 //https://leetcode.com/problems/permutations/description/
 //Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 public class Permutations {
     public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
         boolean[] used = new boolean[nums.length];
-        Arrays.fill(used, false);
-        Stack<Integer> perms = new Stack<Integer>();
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        recur(res, perms, used, nums);
+        rec(res, new ArrayList<>(), nums, used);
         return res;
     }
 
-    private static void recur(List<List<Integer>> res, Stack<Integer> perms, boolean[] used, int[] nums) {
-        if (perms.size() == used.length) {
-            res.add(new ArrayList<Integer>(perms));
+    private static void rec(List<List<Integer>> res, List<Integer> perm, int[] nums, boolean[] used) {
+        if (perm.size() == nums.length) {
+            res.add(new ArrayList<>(perm));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (!used[i]) {
-                used[i] = true;
-                perms.push(nums[i]);
-                recur(res, perms, used, nums);
-                used[i] = false;
-                perms.pop();
+            if (used[i]) {
+                continue;
             }
+            used[i] = true;
+            perm.add(nums[i]);
+            rec(res, perm, nums, used);
+            used[i] = false;
+            perm.remove(perm.size() - 1);
         }
     }
 }
