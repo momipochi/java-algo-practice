6d8bfe5bda0c899f42c0618ca30015fcefac5667
unknown <k25650727@gmail.com>
2024-08-10 16:06:39 +0200

    commit

--- src/main/java/com/example/LeetCode/L53_MaximumSubarray/MaximumSubarray.java
+++ src/main/java/com/example/LeetCode/L53_MaximumSubarray/MaximumSubarray.java
@@ -3,9 +3,6 @@ package com.example.LeetCode.L53_MaximumSubarray;
 // https://leetcode.com/problems/maximum-subarray/
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
-        if (nums.length < 2) {
-            return nums[0];
-        }
         int max = nums[0], sum = 0;
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
