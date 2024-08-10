329e7af4266a090cfe26f94fff7f4faf33591b88
unknown <k25650727@gmail.com>
2024-08-10 10:58:03 +0200

    commit

--- src/main/java/com/example/LeetCode/L55_JumpGame/JumpGame.java
+++ src/main/java/com/example/LeetCode/L55_JumpGame/JumpGame.java
@@ -5,11 +5,11 @@ public class JumpGame {
     public boolean canJump(int[] nums) {
         int reach = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (i > reach)
-                return false;
-            reach = Math.max(reach, i + nums[i]);
-
+            if (reach >= nums.length - 1) {
+                return true;
+            }
+            reach = Math.max(reach, nums[i] + i);
         }
-        return true;
+        return false;
     }
 }
