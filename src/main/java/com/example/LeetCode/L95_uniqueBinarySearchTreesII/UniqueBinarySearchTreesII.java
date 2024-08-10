79f23dabd190da2c6458bf59a99518bb8a1a7dc1
unknown <k25650727@gmail.com>
2024-08-10 15:07:17 +0200

    commit

--- src/main/java/com/example/LeetCode/L95_uniqueBinarySearchTreesII/UniqueBinarySearchTreesII.java
+++ src/main/java/com/example/LeetCode/L95_uniqueBinarySearchTreesII/UniqueBinarySearchTreesII.java
@@ -1,44 +1,37 @@
 package com.example.LeetCode.L95_uniqueBinarySearchTreesII;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import com.example.classes.Coord;
 import com.example.classes.TreeNode;
 
 // https://leetcode.com/problems/unique-binary-search-trees-ii/
 public class UniqueBinarySearchTreesII {
 
     public static List<TreeNode> generateTrees(int n) {
-
-        return generateSubtrees(new Coord(1, n), new HashMap<>());
+        return n > 0 ? generate_trees(1, n) : new ArrayList<>();
     }
 
-    private static List<TreeNode> generateSubtrees(Coord coord, Map<Coord, List<TreeNode>> map) {
-        List<TreeNode> res = new ArrayList<>();
-        if (coord.x > coord.y) {
-            res.add(null);
-            return res;
-        }
-        if (map.containsKey(coord)) {
-            return map.get(coord);
+    private static List<TreeNode> generate_trees(int start, int end) {
+        List<TreeNode> all_trees = new ArrayList<>();
+        if (start > end) {
+            all_trees.add(null);
+            return all_trees;
         }
 
-        for (int i = coord.x; i <= coord.y; i++) {
-            List<TreeNode> left = generateSubtrees(new Coord(coord.x, i - 1), map);
-            List<TreeNode> right = generateSubtrees(new Coord(i + 1, coord.y), map);
-            for (TreeNode l : left) {
-                for (TreeNode r : right) {
-                    TreeNode node = new TreeNode(i, l, r);
-                    res.add(node);
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> left_trees = generate_trees(start, i - 1);
+            List<TreeNode> right_trees = generate_trees(i + 1, end);
+
+            for (TreeNode l : left_trees) {
+                for (TreeNode r : right_trees) {
+                    TreeNode current_tree = new TreeNode(i);
+                    current_tree.left = l;
+                    current_tree.right = r;
+                    all_trees.add(current_tree);
                 }
             }
-            map.putIfAbsent(coord, res);
         }
-
-        return res;
+        return all_trees;
     }
 
 }
