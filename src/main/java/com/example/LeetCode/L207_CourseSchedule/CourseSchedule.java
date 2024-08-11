ca7d25e94da9bd925a070696e7277695214ed97b
unknown <k25650727@gmail.com>
2024-08-11 14:47:27 +0200

    commit

--- src/main/java/com/example/LeetCode/L207_CourseSchedule/CourseSchedule.java
+++ src/main/java/com/example/LeetCode/L207_CourseSchedule/CourseSchedule.java
@@ -86,26 +86,27 @@ public class CourseSchedule {
     }
 
     public boolean canFinishFurtherOptimized(int numCourses, int[][] prerequisites) {
-        final int[] prereqCount = new int[numCourses];
-        for (int[] req : prerequisites)
-            prereqCount[req[1]]++;
-
-        final int length = prerequisites.length;
-        final boolean[] finished = new boolean[length];
+        int[] req = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+            req[prerequisites[i][1]]++;
+        }
+        int length = prerequisites.length;
+        boolean[] visited = new boolean[length];
         boolean run = true;
         while (run) {
             run = false;
             for (int i = 0; i < length; i++) {
-                if (!finished[i] && prereqCount[prerequisites[i][0]] == 0) {
-                    finished[i] = true;
-                    prereqCount[prerequisites[i][1]]--;
+                if (!visited[i] && req[prerequisites[i][0]] == 0) {
                     run = true;
+                    req[prerequisites[i][1]]--;
+                    visited[i] = true;
                 }
             }
         }
         for (int i = 0; i < numCourses; i++) {
-            if (prereqCount[i] != 0)
+            if (req[i] != 0) {
                 return false;
+            }
         }
         return true;
     }
