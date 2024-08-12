324a758bcf974dbf11612e242b62396af5bdce16
momipochi <k25650727@gmail.com>
2024-08-12 22:22:45 +0200

    commit

--- src/main/java/com/example/LeetCode/L695_MaxAreaOfIsland/MaxAreaOfIsland.java
+++ src/main/java/com/example/LeetCode/L695_MaxAreaOfIsland/MaxAreaOfIsland.java
@@ -2,36 +2,29 @@ package com.example.LeetCode.L695_MaxAreaOfIsland;
 
 public class MaxAreaOfIsland {
     public int maxAreaOfIsland(int[][] grid) {
-        int rect=0;
-        int m=grid.length;
-        int n=grid[0].length;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                int rec=helper(grid,i,j,m,n);
-                if(rect<rec)
-                    rect=rec;
+        int row = grid.length, col = grid[0].length, max = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    max = Math.max(max, helper(i, j, row, col, grid));
+                }
             }
         }
-        return rect;
+        return max;
     }
-     public int helper(int[][]grid,int i,int j,int m, int n)
-    {
-        if(i<0||j<0||i>=m||j>=n)
-        {
+
+    public int helper(int i, int j, int maxI, int maxJ, int[][] grid) {
+        if (i < 0 || j < 0 || i >= maxI || j >= maxJ || grid[i][j] == 0) {
             return 0;
         }
-        if(grid[i][j]==0)
-            return 0;
-        grid[i][j]=0;
-        int rect=1;
-        rect+=helper(grid,i-1,j,m,n);
-        rect+=helper(grid,i,j-1,m,n);
-        rect+=helper(grid,i+1,j,m,n);
-        rect+=helper(grid,i,j+1,m,n);
+        grid[i][j] = 0;
 
-        return rect;
+        int res = 1;
+        res+=helper(i+1, j, maxI, maxJ, grid);
+        res+=helper(i-1, j, maxI, maxJ, grid);
+        res+=helper(i, j+1, maxI, maxJ, grid);
+        res+=helper(i, j-1, maxI, maxJ, grid);
 
+        return res;
     }
 }
