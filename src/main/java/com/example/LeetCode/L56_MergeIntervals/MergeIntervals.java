c7f128c30297f3b4fe4f12362392894aaa9ec1a8
unknown <k25650727@gmail.com>
2024-08-22 16:20:48 +0200

    commit

--- src/main/java/com/example/LeetCode/L56_MergeIntervals/MergeIntervals.java
+++ src/main/java/com/example/LeetCode/L56_MergeIntervals/MergeIntervals.java
@@ -7,15 +7,15 @@ import java.util.Stack;
 public class MergeIntervals {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        Stack<int[]> stack = new Stack<>();
         int[] prev = intervals[0];
+        Stack<int[]> stack = new Stack<>();
 
         for (int i = 0; i < intervals.length; i++) {
-            if (prev[1] >= intervals[i][0]) {
-                prev[1] = Math.max(prev[1], intervals[i][1]);
-            } else {
+            if (prev[1] < intervals[i][0]) {
                 stack.add(prev);
                 prev = intervals[i];
+            } else {
+                prev[1] = Math.max(prev[1], intervals[i][1]);
             }
         }
         stack.add(prev);
