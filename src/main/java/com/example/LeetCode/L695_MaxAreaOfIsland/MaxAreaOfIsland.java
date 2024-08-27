84369537992d4d7775c4d6a9f9d5900fae671a1b
unknown <k25650727@gmail.com>
2024-08-27 15:44:15 +0200

    commit

--- src/main/java/com/example/LeetCode/L695_MaxAreaOfIsland/MaxAreaOfIsland.java
+++ src/main/java/com/example/LeetCode/L695_MaxAreaOfIsland/MaxAreaOfIsland.java
@@ -2,29 +2,27 @@ package com.example.LeetCode.L695_MaxAreaOfIsland;
 
 public class MaxAreaOfIsland {
     public int maxAreaOfIsland(int[][] grid) {
-        int row = grid.length, col = grid[0].length, max = 0;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        int max = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == 1) {
-                    max = Math.max(max, helper(i, j, row, col, grid));
+                    max = Math.max(max, solution(i, j, 0, grid));
                 }
             }
         }
         return max;
     }
 
-    public int helper(int i, int j, int maxI, int maxJ, int[][] grid) {
-        if (i < 0 || j < 0 || i >= maxI || j >= maxJ || grid[i][j] == 0) {
-            return 0;
+    private int solution(int i, int j, int sum, int[][] grid) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != 1) {
+            return sum;
         }
         grid[i][j] = 0;
-
-        int res = 1;
-        res+=helper(i+1, j, maxI, maxJ, grid);
-        res+=helper(i-1, j, maxI, maxJ, grid);
-        res+=helper(i, j+1, maxI, maxJ, grid);
-        res+=helper(i, j-1, maxI, maxJ, grid);
-
-        return res;
+        int tmp = sum + 1;
+        tmp = Math.max(tmp, solution(i + 1, j, tmp, grid));
+        tmp = Math.max(tmp, solution(i - 1, j, tmp, grid));
+        tmp = Math.max(tmp, solution(i, j + 1, tmp, grid));
+        tmp = Math.max(tmp, solution(i, j - 1, tmp, grid));
+        return tmp;
     }
 }
