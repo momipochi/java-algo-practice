e467912c12b310ffc346173c046c7754d679a637
unknown <k25650727@gmail.com>
2024-08-28 10:58:47 +0200

    commit

--- src/main/java/com/example/LeetCode/L463_IslandPerimeter/IslandPerimeter.java
+++ src/main/java/com/example/LeetCode/L463_IslandPerimeter/IslandPerimeter.java
@@ -1,13 +1,19 @@
 package com.example.LeetCode.L463_IslandPerimeter;
 
+// https://leetcode.com/problems/island-perimeter/description/
+
+/*
+ * For every 1 encountered is an island, check row+1 and col+1 for neighbors,
+ * in the end caculate it by island*4-neighbours*2
+ */
+
 public class IslandPerimeter {
     public int islandPerimeter(int[][] grid) {
-        int islands = 0, neighbours = 0;
-
+        int island = 0, neighbours = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == 1) {
-                    islands++;
+                    island++;
                     if (i < grid.length - 1 && grid[i + 1][j] == 1) {
                         neighbours++;
                     }
@@ -17,7 +23,6 @@ public class IslandPerimeter {
                 }
             }
         }
-
-        return islands * 4 - neighbours * 2;
+        return island * 4 - neighbours * 2;
     }
 }
