a251e07f10d1101a4da24ec82b15a4ba4bf9c7eb
unknown <k25650727@gmail.com>
2024-08-28 08:45:13 +0200

    commit

--- src/main/java/com/example/LeetCode/L695_MaxAreaOfIsland/MaxAreaOfIsland.java
+++ src/main/java/com/example/LeetCode/L695_MaxAreaOfIsland/MaxAreaOfIsland.java
@@ -1,5 +1,15 @@
 package com.example.LeetCode.L695_MaxAreaOfIsland;
 
+// https://leetcode.com/problems/max-area-of-island/description/
+
+/*
+ * The idea of the algorithm for this task is to iterate through the grid to find the first 1 in the array
+ * once we find it, we check its neighbours while also changing the current index element to 0,
+ * this way we can, by design, prevent encountering the same 1s again.
+ * 
+ * All that needs to be done afterwards is to check for the previous maximum with the current maximum.
+ */
+
 public class MaxAreaOfIsland {
     public int maxAreaOfIsland(int[][] grid) {
         int max = 0;
