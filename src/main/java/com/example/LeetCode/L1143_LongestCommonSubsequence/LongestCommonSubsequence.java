40b53fb8c97b1452fdf70a340f01c9323c0810cf
unknown <k25650727@gmail.com>
2024-08-28 09:10:20 +0200

    commit

--- src/main/java/com/example/LeetCode/L1143_LongestCommonSubsequence/LongestCommonSubsequence.java
+++ src/main/java/com/example/LeetCode/L1143_LongestCommonSubsequence/LongestCommonSubsequence.java
@@ -4,23 +4,26 @@ package com.example.LeetCode.L1143_LongestCommonSubsequence;
 
 /*
  * The idea is that, by using a grid we can create a history of every index, each of these indexes can also
- * be used to refer back to the position of the strings.
+ * be used to refer back to the position of the strings. Looping from the end to 0 inclusive.
  * 
  * All we have to do is to check if the character and the given indexs are the same, if so we can go diagonoally,
  * otherwise we need to check whether going up or left returns a greater value.
  * In the end the answer would be carried to [0][0].
  * 
  * We just need to make sure to have the grid to be 1 row and col larger to compensate for a starting point.
+ * You should also consider declaring variables holding the length of the strings.
+ * 
+ * Tip: it is faster to convert string to char array and getting the element by index than using string charAt method
  */
 
 public class LongestCommonSubsequence {
     public int longestCommonSubsequence(String text1, String text2) {
-        int t1 = text1.length(), t2 = text2.length();
-        int[][] grid = new int[t1 + 1][t2 + 1];
-
-        for (int i = t1 - 1; i >= 0; i--) {
-            for (int j = t2 - 1; j >= 0; j--) {
-                if (text1.charAt(i) == text2.charAt(j)) {
+        int l = text1.length(), r = text2.length();
+        char[] t1 = text1.toCharArray(), t2 = text2.toCharArray();
+        int[][] grid = new int[l + 1][r + 1];
+        for (int i = l - 1; i >= 0; i--) {
+            for (int j = r - 1; j >= 0; j--) {
+                if (t1[i] == t2[j]) {
                     grid[i][j] = grid[i + 1][j + 1] + 1;
                 } else {
                     grid[i][j] = Math.max(grid[i + 1][j], grid[i][j + 1]);
