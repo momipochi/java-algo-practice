09b1e26ccfcf10bf298954ce879db50fd81102de
unknown <k25650727@gmail.com>
2024-09-10 17:19:41 +0200

    commit

--- src/main/java/com/example/LeetCode/L463_IslandPerimeter/IslandPerimeter.java
+++ src/main/java/com/example/LeetCode/L463_IslandPerimeter/IslandPerimeter.java
@@ -9,20 +9,20 @@ package com.example.LeetCode.L463_IslandPerimeter;
 
 public class IslandPerimeter {
     public int islandPerimeter(int[][] grid) {
-        int island = 0, neighbours = 0;
+        int island = 0, neighbors = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == 1) {
                     island++;
                     if (i < grid.length - 1 && grid[i + 1][j] == 1) {
-                        neighbours++;
+                        neighbors++;
                     }
-                    if (j < grid[i].length - 1 && grid[i][j + 1] == 1) {
-                        neighbours++;
+                    if (j < grid[i].length && grid[i][j + 1] == 1) {
+                        neighbors++;
                     }
                 }
             }
         }
-        return island * 4 - neighbours * 2;
+        return island * 4 - neighbors * 2;
     }
 }
