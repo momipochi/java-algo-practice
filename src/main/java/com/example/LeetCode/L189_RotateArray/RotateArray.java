8d234a6c1d551efbd0099de004f7112f33238a37
unknown <k25650727@gmail.com>
2024-09-11 16:40:50 +0200

    commit

--- src/main/java/com/example/LeetCode/L189_RotateArray/RotateArray.java
+++ src/main/java/com/example/LeetCode/L189_RotateArray/RotateArray.java
@@ -1,6 +1,16 @@
 package com.example.LeetCode.L189_RotateArray;
 
-// https://leetcode.com/problems/rotate-array/solutions/5158183/java-easy-to-understand-solution-with-explanation-100-beats/
+// https://leetcode.com/problems/rotate-array/description/
+
+/*
+ * The idea of the algorithm is to use a characteristic of reversal.
+ * Instead of rotating an array one by one, we can simply find the index
+ * to which the entire array must rotate pass. We can easily find the value
+ * by modding the length of the array. All we have to do next is simply
+ * reverse the entire array, reverse until the modular - 1, and reverse
+ * from modular to the end.
+ */
+
 public class RotateArray {
     public void rotate(int[] nums, int k) {
         k %= nums.length;
