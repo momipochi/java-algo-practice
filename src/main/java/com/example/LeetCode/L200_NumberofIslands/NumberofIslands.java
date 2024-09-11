6e38e8b90eb68f2acfcd42e9910952bae301aef7
unknown <k25650727@gmail.com>
2024-09-11 16:33:25 +0200

    commit

--- src/main/java/com/example/LeetCode/L200_NumberofIslands/NumberofIslands.java
+++ src/main/java/com/example/LeetCode/L200_NumberofIslands/NumberofIslands.java
@@ -10,11 +10,11 @@ package com.example.LeetCode.L200_NumberofIslands;
 
 public class NumberofIslands {
     public int numIslands(char[][] grid) {
-        int res = 0, row = grid.length, col = grid[0].length;
+        int res = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == '1') {
-                    fixGrid(i, j, row, col, grid);
+                    dfs(i, j, grid);
                     res++;
                 }
             }
@@ -22,15 +22,15 @@ public class NumberofIslands {
         return res;
     }
 
-    private void fixGrid(int i, int j, int maxI, int maxJ, char[][] grid) {
-        if (i < 0 || j < 0 || i >= maxI || j >= maxJ || grid[i][j] == '0') {
+    private void dfs(int x, int y, char[][] grid) {
+        if (x < 0 || y < 0 || x >= grid.length || y >= grid[x].length || grid[x][y] != '1') {
             return;
         }
-        grid[i][j] = '0';
-        fixGrid(i + 1, j, maxI, maxJ, grid);
-        fixGrid(i - 1, j, maxI, maxJ, grid);
-        fixGrid(i, j + 1, maxI, maxJ, grid);
-        fixGrid(i, j - 1, maxI, maxJ, grid);
+        grid[x][y] = '0';
+        dfs(x + 1, y, grid);
+        dfs(x - 1, y, grid);
+        dfs(x, y + 1, grid);
+        dfs(x, y - 1, grid);
     }
 
 }
