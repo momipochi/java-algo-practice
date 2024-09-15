44c78b01a33567b7e341c975684c67933dee957c
unknown <k25650727@gmail.com>
2024-09-15 20:14:42 +0200

    commit

--- src/main/java/com/example/LeetCode/L55_JumpGame/JumpGame.java
+++ src/main/java/com/example/LeetCode/L55_JumpGame/JumpGame.java
@@ -5,20 +5,21 @@ package com.example.LeetCode.L55_JumpGame;
 /*
  * We only need to know if we can reach the destination, meaning we should have a variable
  * that would add up all the elements. We only need to add the current index to the current element
- * and compare it with the variable to get the max. This way we are able to ensure that
+ * and compare it with the current index, if the current index is larger than the variable, this naturally
+ * can only happen if there are no elements that add up enough to surpass the index. This way we are able to ensure that
  * we go through all elements and check all the values, which one is greater and whether
  * it can even reach the next index.
  */
 
 public class JumpGame {
     public boolean canJump(int[] nums) {
-        int reach = 0;
+        int res = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (reach >= nums.length - 1) {
-                return true;
+            if (i > res) {
+                return false;
             }
-            reach = Math.max(reach, nums[i] + i);
+            res = res > nums[i] + 1 ? res : nums[i] + 1;
         }
-        return false;
+        return true;
     }
 }
