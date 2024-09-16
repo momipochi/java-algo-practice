f5a93d0603524e51320b1d8caa40c2ad4d07ed18
unknown <k25650727@gmail.com>
2024-09-16 08:34:29 +0200

    commit

--- src/main/java/com/example/LeetCode/L77_combinations/Combinations.java
+++ src/main/java/com/example/LeetCode/L77_combinations/Combinations.java
@@ -6,6 +6,16 @@ import java.util.List;
 
 // Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
 // You may return the answer in any order.
+
+/*
+ * Unlike permutations where always start looping from 0, with combinations,
+ * we cannot do that, because we want unique combinations, regardless of order.
+ * What we can do in this case is simply pass a starting index in the recursive
+ * call. This way we can by design rule out duplicates, since we won't be starting
+ * from 0 everytime. There is no need to even check if the colleciton contains
+ * an element already.
+ */
+
 public class Combinations {
     public static List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> res = new ArrayList<>();
