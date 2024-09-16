18a8a8213b10e78ca17ba5938ebff0699f14b982
unknown <k25650727@gmail.com>
2024-09-16 08:03:21 +0200

    commit

--- src/main/java/com/example/LeetCode/L64_MinPathSum/MinPathSum.java
+++ src/main/java/com/example/LeetCode/L64_MinPathSum/MinPathSum.java
@@ -1,5 +1,18 @@
 package com.example.LeetCode.L64_MinPathSum;
 
+// https://leetcode.com/problems/minimum-path-sum/description/
+
+/*
+ * THe algorithm uses tabulation. Naturally, this usually means you need additional
+ * row or col. For this case we want to start iterating from the end so we start from
+ * the grid.length - 1 rather than our tabulation's length to compensate for the 
+ * additional size. If our indexes both reach 0 we can simply return the element
+ * on grid at that index. If the indexs are lower than 0, we return Integer.MaxValue.
+ * If the element in the tab at index is not 0, we return that element.
+ * In the end, we assign the current tab element to current grid element plus
+ * the smaller value from the next row or col, from the recursive call.
+ */
+
 public class MinPathSum {
     int[][] tab;
 
@@ -21,22 +34,22 @@ public class MinPathSum {
 
     }
 
-
     public int minPathSumFromZero(int[][] grid) {
-        int dp[][] = new int[grid.length+1][grid[0].length+1];
-        return sum(grid,0,0,dp);    
+        int dp[][] = new int[grid.length + 1][grid[0].length + 1];
+        return sum(grid, 0, 0, dp);
     }
-    public int sum(int arr[][], int i, int j, int dp[][]){
-        if(i>=arr.length||j>=arr[0].length){
+
+    public int sum(int arr[][], int i, int j, int dp[][]) {
+        if (i >= arr.length || j >= arr[0].length) {
             return Integer.MAX_VALUE;
         }
-        if(i==arr.length-1&&j==arr[0].length-1){
+        if (i == arr.length - 1 && j == arr[0].length - 1) {
             return arr[i][j];
         }
-        if(dp[i][j]!=0){
+        if (dp[i][j] != 0) {
             return dp[i][j];
         }
-        
-        return dp[i][j] =arr[i][j] + Math.min(sum(arr,i,j+1,dp),sum(arr,i+1,j,dp));
+
+        return dp[i][j] = arr[i][j] + Math.min(sum(arr, i, j + 1, dp), sum(arr, i + 1, j, dp));
     }
 }
