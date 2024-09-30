05de60e01cf74f62f95ecc53ad26707c388bb20d
unknown <k25650727@gmail.com>
2024-09-30 11:12:59 +0200

    commit

--- src/main/java/com/example/LeetCode/L1466_ReorderRoutes/ReorderRoutes.java
+++ src/main/java/com/example/LeetCode/L1466_ReorderRoutes/ReorderRoutes.java
@@ -1,68 +1,72 @@
 package com.example.LeetCode.L1466_ReorderRoutes;
 
+import java.util.Stack;
+
 // https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/description/
 
 /*
- * The idea of the algorithm is to bfs approach. For each point we check whether the connections 
- * lead back, or in this impelmentation, we check using an array of boolean if a point has been visited.
- * This is possible because the connections are guaranteed to start from 0 and there is no skipping to
- * the next number, meaning we can use the values of each point as an index to easily mark the element
- * in the boolean array as true, aka visited.
+ * We start by defining an array of booleans that keeps track of visited cities and marking index 0 as true to jump start the algorithm.
+ * We need to stacks in this solution, this first we will use at the beginning, the other later. We want to first iterate through all the
+ * cities by looking into the connections. For every connection we check the FROM and TO index. If FROM index has been visited, this means
+ * we need to increment counter and mark TO index as true. If TO index is visited then this naturally means FROM must have been visited as well
+ * so we mark FROM index as true. If neither conditions match we add this index to the stack for later.
  * 
- * When running recursive function, we check if the TO index is marked as true in the boolean array. If
- * true, this means that FROM has also been visited and we can return early. If instead FROM was visited, this means that TO
- * needs to be reordered. If neither are the case we need to run the search again recrusively for the next
- * city.
+ * Once we finish, we need to declare the 2nd stack. While stack1 (s1) or stack2 (s2) isn't empty, we want to check both stacks one in order.
+ * We check first s1, while s1 isn't empty we do the same check we did earlier, except this time we push the index to s2.
+ * We repeat the same action with s2, of course, we push the index to s1 instead.
  * 
- * There are two important things to remember. First, it is possible that a point is reachable, hence
- * we return from the function early and never reach the recursion part of the function, meaning we will
- * not iterate through all points properly. The solution is to simply iterate from 0 to n (number of cities)
- * and compute for each city.
- * 
- * Secondly, it is possible that a city turns out to be reachable if we traverse further down, so after the
- * recursion, we need to make the same check once again to make sure that any changes made during the recursion,
- * we verify if those "future actions" had any effects to current cities.
+ * In the end we return the count;
  */
 
+//previous solution hash 5ef16150a0177b015388cb963e40a63feeab0c2b
+
 public class ReorderRoutes {
-    boolean[] visited;
-    int reorders = 0;
-
     public int minReorder(int n, int[][] connections) {
-        visited = new boolean[n + 1];
-        visited[0] = true;
-        for (int i = 0; i < n; i++) {
-            search(i, connections);
-        }
-        return reorders;
-    }
+        boolean[] reach = new boolean[n];
+        int rows = connections.length;
+        reach[0] = true;
+        int count = 0;
 
-    private void search(int city, int[][] connections) {
-        if (city >= connections.length || city < 0) {
-            return;
-        }
-        int from = connections[city][0], to = connections[city][1];
-        if (visited[to]) {
-            visited[from] = true;
-            return;
-        }
-        if (visited[from]) {
-            reorders++;
-            visited[to] = true;
-        } else {
-            search(city + 1, connections);
-            if (visited[to]) {
-                visited[from] = true;
-                return;
-            }
-            if (visited[from]) {
-                reorders++;
-                visited[to] = true;
+        Stack<Integer> s1 = new Stack<>();
+        for (int i = 0; i < rows; i++) {
+            if (reach[connections[i][0]]) {
+                count++;
+                reach[connections[i][1]] = true;
+            } else if (reach[connections[i][1]]) {
+                reach[connections[i][0]] = true;
             } else {
-                search(city - 1, connections);
+                s1.push(i);
             }
         }
 
-    }
+        Stack<Integer> s2 = new Stack<>();
 
+        while (!s1.isEmpty() || !s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                int i = s1.pop();
+                if (reach[connections[i][0]]) {
+                    count++;
+                    reach[connections[i][1]] = true;
+                } else if (reach[connections[i][1]]) {
+                    reach[connections[i][0]] = true;
+                } else {
+
+                    s2.push(i);
+                }
+
+            }
+
+            while (!s2.isEmpty()) {
+                int i = s2.pop();
+                if (reach[connections[i][0]]) {
+                    count++;
+                    reach[connections[i][1]] = true;
+                } else if (reach[connections[i][1]]) {
+                    reach[connections[i][0]] = true;
+                } else
+                    s1.push(i);
+            }
+        }
+        return count;
+    }
 }
