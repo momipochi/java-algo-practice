61d7ff22b0a63ed9dd020ae8368c227edbfbfca7
unknown <k25650727@gmail.com>
2024-10-17 12:29:45 +0200

    commit

--- src/main/java/com/example/LeetCode/L443_StringCompression/StringCompression.java
+++ src/main/java/com/example/LeetCode/L443_StringCompression/StringCompression.java
@@ -4,29 +4,27 @@ package com.example.LeetCode.L443_StringCompression;
 
 public class StringCompression {
     public int compress(char[] chars) {
-        StringBuilder sb = new StringBuilder();
-        int i = 0;
+        int i = 0, size = 0, currI = 0;
         char curr = chars[i];
         while (i < chars.length) {
-            System.out.println("i: " + i + " length: " + chars.length);
-            sb.append(chars[i]);
+            chars[currI] = chars[i];
+            currI++;
+            size++;
             int count = 0;
             while (i < chars.length && chars[i] == curr) {
                 count++;
                 i++;
             }
             if (count > 1) {
-                sb.append(count);
+                chars[currI] = (char) count;
+                currI++;
+                size++;
             }
             if (i < chars.length) {
                 curr = chars[i];
             }
             count = 0;
         }
-        char[] tmp = sb.toString().toCharArray();
-        for (int j = 0; j < tmp.length; j++) {
-            chars[j] = tmp[j];
-        }
-        return tmp.length;
+        return size;
     }
 }
