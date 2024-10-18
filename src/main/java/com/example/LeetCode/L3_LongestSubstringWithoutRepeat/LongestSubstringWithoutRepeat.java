9e73eadfdd8e8f944d1b2437efbf15bd895aa6c9
unknown <k25650727@gmail.com>
2024-10-18 08:55:04 +0200

    commit

--- src/main/java/com/example/LeetCode/L3_LongestSubstringWithoutRepeat/LongestSubstringWithoutRepeat.java
+++ src/main/java/com/example/LeetCode/L3_LongestSubstringWithoutRepeat/LongestSubstringWithoutRepeat.java
@@ -1,12 +1,14 @@
 package com.example.LeetCode.L3_LongestSubstringWithoutRepeat;
 
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
 public class LongestSubstringWithoutRepeat {
     public int lengthOfLongestSubstring(String s) {
         char[] arr = s.toCharArray();
         int l = 0, r = 0, max = 0;
 
         while (r < arr.length) {
-            int offset = existsChar(arr, arr[r], l, r);
+            int offset = getOffset(arr, arr[r], l, r);
             if (offset != 0) {
                 l += offset;
                 r++;
@@ -20,7 +22,7 @@ public class LongestSubstringWithoutRepeat {
         return max;
     }
 
-    private int existsChar(char[] arr, char c, int l, int r) {
+    private int getOffset(char[] arr, char c, int l, int r) {
         int count = 1;
         for (int i = l; i < r; i++) {
             if (arr[i] == c) {
