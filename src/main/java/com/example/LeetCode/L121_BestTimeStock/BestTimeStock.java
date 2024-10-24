96b08898e59849edb4413b6543be4f9cfb586b37
unknown <k25650727@gmail.com>
2024-10-24 16:57:28 +0200

    commit

--- src/main/java/com/example/LeetCode/L121_BestTimeStock/BestTimeStock.java
+++ src/main/java/com/example/LeetCode/L121_BestTimeStock/BestTimeStock.java
@@ -1,5 +1,6 @@
 package com.example.LeetCode.L121_BestTimeStock;
 
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
 public class BestTimeStock {
     public int maxProfit(int[] prices) {
         int max = 0, l = prices[0];
--- src/main/java/com/example/LeetCode/L122_BestTImeStockII/BestTImeStockII.java
+++ src/main/java/com/example/LeetCode/L122_BestTImeStockII/BestTImeStockII.java
@@ -1,5 +1,6 @@
 package com.example.LeetCode.L122_BestTImeStockII;
 
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 public class BestTImeStockII {
     public int maxProfit(int[] prices) {
         int profit = 0;
