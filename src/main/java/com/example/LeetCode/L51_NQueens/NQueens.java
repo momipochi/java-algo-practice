3ccde7c52dda4ac816e13f6a633ac98a5cb5d748
unknown <k25650727@gmail.com>
2024-10-26 11:26:22 +0200

    commit

--- src/main/java/com/example/LeetCode/L51_NQueens/NQueens.java
+++ src/main/java/com/example/LeetCode/L51_NQueens/NQueens.java
@@ -1,55 +1,46 @@
 package com.example.LeetCode.L51_NQueens;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // https://leetcode.com/problems/n-queens/
 
 public class NQueens {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> res = new ArrayList<>();
-        start(res, new ArrayList<>(), 0, n);
-        return res;
+        List<List<String>> ans = new ArrayList<>();
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; ++i)
+            Arrays.fill(board[i], '.');
+
+        dfs(n, 0, new boolean[n], new boolean[2 * n - 1], new boolean[2 * n - 1], board, ans);
+        return ans;
     }
 
-    private void start(List<List<String>> res, List<Integer> pos, int row, int n) {
-        if (row == n) {
-            res.add(formString(pos));
+    private void dfs(int n, int i, boolean[] cols, boolean[] diag1, boolean[] diag2, char[][] board,
+            List<List<String>> ans) {
+        if (i == n) {
+            ans.add(construct(board));
+            return;
         }
-        for (int col = 0; col < n; col++) {
-            if (isValid(pos, row, col)) {
-                pos.add(col);
-                start(res, pos, row + 1, n);
-                pos.remove(pos.size() - 1);
-            }
+
+        for (int j = 0; j < cols.length; ++j) {
+            if (cols[j] || diag1[i + j] || diag2[j - i + n - 1])
+                continue;
+            board[i][j] = 'Q';
+            cols[j] = diag1[i + j] = diag2[j - i + n - 1] = true;
+            dfs(n, i + 1, cols, diag1, diag2, board, ans);
+            cols[j] = diag1[i + j] = diag2[j - i + n - 1] = false;
+            board[i][j] = '.';
         }
     }
 
-    private boolean isValid(List<Integer> pos, int row, int col) {
-        for (int rowTaken = 0; rowTaken < pos.size(); rowTaken++) {
-            int colTaken = pos.get(rowTaken);
-            if (col == colTaken || Math.abs(row - rowTaken) == Math.abs(col - colTaken)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private List<String> formString(List<Integer> pos) {
-        List<String> res = new ArrayList<>();
-        int n = pos.size();
-        for (int i : pos) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < n; j++) {
-                if (j == i) {
-                    sb.append("Q");
-                } else {
-                    sb.append(".");
-                }
-            }
-            res.add(sb.toString());
-        }
-        return res;
+    private List<String> construct(char[][] board) {
+        List<String> listBoard = new ArrayList<>();
+        for (int i = 0; i < board.length; ++i)
+            listBoard.add(String.valueOf(board[i]));
+        return listBoard;
     }
 
 }
