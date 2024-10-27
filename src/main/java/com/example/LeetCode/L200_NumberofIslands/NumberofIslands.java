b9d0eecc7c73054d355208f332270f235d705b7e
unknown <k25650727@gmail.com>
2024-10-27 19:31:48 +0100

    commit

--- src/main/java/com/example/LeetCode/L200_NumberofIslands/NumberofIslands.java
+++ src/main/java/com/example/LeetCode/L200_NumberofIslands/NumberofIslands.java
@@ -9,28 +9,31 @@ package com.example.LeetCode.L200_NumberofIslands;
  */
 
 public class NumberofIslands {
+    char[][] grid;
+
     public int numIslands(char[][] grid) {
         int res = 0;
+        this.grid = grid;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == '1') {
                     res++;
-                    modifyIsland(i, j, grid);
+                    rec(i, j);
                 }
             }
         }
         return res;
     }
 
-    private void modifyIsland(int x, int y, char[][] grid) {
-        if (x < 0 || y < 0 || x >= grid.length || y >= grid[x].length || grid[x][y] == '0') {
+    private void rec(int x, int y) {
+        if (x < 0 || y < 0 || x >= grid.length || y >= grid[x].length || grid[x][y] != '1') {
             return;
         }
         grid[x][y] = '0';
-        modifyIsland(x + 1, y, grid);
-        modifyIsland(x, y + 1, grid);
-        modifyIsland(x, y - 1, grid);
-        modifyIsland(x - 1, y, grid);
+        rec(x + 1, y);
+        rec(x - 1, y);
+        rec(x, y + 1);
+        rec(x, y - 1);
     }
 
 }
