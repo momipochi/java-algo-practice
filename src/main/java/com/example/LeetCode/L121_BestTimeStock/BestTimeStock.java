6912a13fec2e5234f9169ba4b1838dfc549c73dd
unknown <k25650727@gmail.com>
2024-10-27 18:02:13 +0100

    commit

--- src/main/java/com/example/LeetCode/L121_BestTimeStock/BestTimeStock.java
+++ src/main/java/com/example/LeetCode/L121_BestTimeStock/BestTimeStock.java
@@ -3,12 +3,13 @@ package com.example.LeetCode.L121_BestTimeStock;
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
 public class BestTimeStock {
     public int maxProfit(int[] prices) {
-        int max = 0, l = prices[0];
+        int max = 0, min = prices[0];
         for (int i = 1; i < prices.length; i++) {
-            if (l > prices[i]) {
-                l = prices[i];
+            if (min > prices[i]) {
+                min = prices[i];
             }
-            max = Math.max(max, prices[i] - l);
+            int tmp = prices[i] - min;
+            max = max > tmp ? max : tmp;
         }
         return max;
     }
