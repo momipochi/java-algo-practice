356be2ce389065d2f48fa62c5492a2153b3fcedd
unknown <k25650727@gmail.com>
2024-10-27 11:19:05 +0100

    commit

--- src/main/java/com/example/LeetCode/L63_UniquePathsII/UniquePathsII.java
+++ src/main/java/com/example/LeetCode/L63_UniquePathsII/UniquePathsII.java
@@ -26,26 +26,22 @@ public class UniquePathsII {
 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
-
-        for (int i = 0; i < obstacleGrid.length; i++) {
-            for (int j = 0; j < obstacleGrid[i].length; j++) {
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                } else {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[i].length; j++) {
+                if (obstacleGrid[i][j] == 0) {
                     if (i == 0 && j == 0) {
                         dp[i][j] = 1;
-                    } else {
-                        if (i > 0) {
-                            dp[i][j] += dp[i - 1][j];
-                        }
-                        if (j > 0) {
-                            dp[i][j] += dp[i][j - 1];
-                        }
+                        continue;
+                    }
+                    if (i > 0) {
+                        dp[i][j] += dp[i - 1][j];
+                    }
+                    if (j > 0) {
+                        dp[i][j] += dp[i][j - 1];
                     }
                 }
             }
         }
-
         return dp[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
     }
 }
