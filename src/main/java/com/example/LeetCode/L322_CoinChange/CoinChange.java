d911fc9170c96c20493dbbb93446529f4b648ef5
unknown <k25650727@gmail.com>
2024-10-27 20:57:04 +0100

    commit

--- src/main/java/com/example/LeetCode/L322_CoinChange/CoinChange.java
+++ src/main/java/com/example/LeetCode/L322_CoinChange/CoinChange.java
@@ -5,6 +5,15 @@ import java.util.Arrays;
 // https://leetcode.com/problems/coin-change/
 public class CoinChange {
     public int coinChange(int[] coins, int amount) {
-        return 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, 1, dp.length, amount + 1);
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 }
