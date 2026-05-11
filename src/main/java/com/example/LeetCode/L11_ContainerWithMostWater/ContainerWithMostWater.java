852a36190882082bea0de92027ead55519f996a3
Alexander Tu <axt@WS-CFD24K4V2W.local>
2026-05-11 04:40:47 +0200

    c

--- src/main/java/com/example/LeetCode/L11_ContainerWithMostWater/ContainerWithMostWater.java
+++ src/main/java/com/example/LeetCode/L11_ContainerWithMostWater/ContainerWithMostWater.java
@@ -3,10 +3,10 @@ package com.example.LeetCode.L11_ContainerWithMostWater;
 // https://leetcode.com/problems/container-with-most-water/description/
 public class ContainerWithMostWater {
     public int maxArea(int[] height) {
-        int l = 0, r = height.length - 1, max = 0;
+        int l = 0, r = height.length - 1, max = 0, curr = 0;
 
         while (l < r) {
-            int curr = (r - l);
+            curr = r - l;
             if (height[l] < height[r]) {
                 curr *= height[l];
                 l++;
@@ -14,7 +14,7 @@ public class ContainerWithMostWater {
                 curr *= height[r];
                 r--;
             }
-            max = max > curr ? max : curr;
+            max = curr > max ? curr : max;
         }
         return max;
     }
