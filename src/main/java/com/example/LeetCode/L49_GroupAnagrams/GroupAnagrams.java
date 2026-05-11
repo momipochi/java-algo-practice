bf25028a0be26520cf05276eb24f17bfe816e07f
Alexander Tu <axt@WS-CFD24K4V2W.local>
2026-05-11 09:01:40 +0200

    c

--- src/main/java/com/example/LeetCode/L49_GroupAnagrams/GroupAnagrams.java
+++ src/main/java/com/example/LeetCode/L49_GroupAnagrams/GroupAnagrams.java
@@ -4,18 +4,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 // https://leetcode.com/problems/group-anagrams/description/
 public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
+        HashMap<String, List<String>> map = new HashMap<>();
         for (int i = 0; i < strs.length; i++) {
-            char[] c = strs[i].toCharArray();
-            Arrays.sort(c);
-            String tmp = new String(c);
-            map.putIfAbsent(tmp, new ArrayList<>());
-            map.get(tmp).add(strs[i]);
+            char[] tmp = strs[i].toCharArray();
+            Arrays.sort(tmp);
+            String sorted = new String(tmp);
+            map.putIfAbsent(sorted, new ArrayList<>());
+            map.get(sorted).add(strs[i]);
         }
         return new ArrayList<>(map.values());
     }
