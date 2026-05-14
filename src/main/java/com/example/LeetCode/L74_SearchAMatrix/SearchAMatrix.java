8267821943dcb1cd28890537a38cdc2512625a60
Alexander Tu <axt@WS-CFD24K4V2W.local>
2026-05-14 03:30:31 +0200

    serarchAMatrix new solution

--- src/main/java/com/example/LeetCode/L74_SearchAMatrix/SearchAMatrix.java
+++ src/main/java/com/example/LeetCode/L74_SearchAMatrix/SearchAMatrix.java
@@ -3,6 +3,27 @@ package com.example.LeetCode.L74_SearchAMatrix;
 // https://leetcode.com/problems/search-a-2d-matrix/description/
 public class SearchAMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
+        int max = matrix[0][0], lastPositionMax = Integer.MIN_VALUE;
+        boolean targetInMatrix = false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (j == 0 && matrix[i][j] < lastPositionMax) {
+                    return false;
+                }
+                if (max > matrix[i][j]) {
+                    return false;
+                }
+                max = matrix[i][j];
+                if (!targetInMatrix) {
+                    targetInMatrix = matrix[i][j] == target;
+                }
+            }
+            lastPositionMax = matrix[i][matrix[i].length - 1];
+        }
+        return true && targetInMatrix;
+    }
+
+    public boolean searchMatrixAlternate(int[][] matrix, int target) {
         for (int i = 0; i < matrix.length; i++) {
             if (target > matrix[i][matrix[0].length - 1]) {
                 continue;
@@ -20,7 +41,6 @@ public class SearchAMatrix {
                 }
                 return false;
             }
-
         }
         return false;
     }
